a76cb6746917823ef29a167649f5775a4181e1b2
Simon Baev <simon.baev@bsci.com>
2023-12-14 12:02:24 -0700

    set layout

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,43 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <RouterView class="viewport" />
+  <div class="layout">
+    <nav class="nav-bar">
+      <RouterLink to="/" class="link">
+        <img src="@/assets/logo-light.svg" alt="logo" class="logo">
+      </RouterLink>
+      <section class="right" />
+    </nav>
+    <main class="viewport">
+      <RouterView />
+    </main>
+  </div>
 </template>
 
-<style scoped>
-  .viewport {
+<style scoped lang="scss">
+  .layout {
     height: 100vh;
+    display: flex;
+    flex-direction: column;
+    .nav-bar {
+      flex-basis: 64px;
+      padding: 0 16px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      border-bottom: 1px solid grey;
+      .link {
+        display: flex;
+        align-items: center;
+      }
+      .logo {
+        height: 32px;
+      }
+    }
+    .viewport {
+      flex: 1 0 0;
+      padding: 1em;
+      overflow-y: hidden
+    }
   }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,76 @@
 </script>
 
 <template>
-  <main>
+  <div class="content">
     <h2>Hello</h2>
-  </main>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ex quidem blanditiis, quae eum similique fugit inventore consectetur voluptates sunt. Mollitia autem, magnam alias rerum omnis nostrum minus maxime assumenda?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ex quidem blanditiis, quae eum similique fugit inventore consectetur voluptates sunt. Mollitia autem, magnam alias rerum omnis nostrum minus maxime assumenda?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ex quidem blanditiis, quae eum similique fugit inventore consectetur voluptates sunt. Mollitia autem, magnam alias rerum omnis nostrum minus maxime assumenda?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ex quidem blanditiis, quae eum similique fugit inventore consectetur voluptates sunt. Mollitia autem, magnam alias rerum omnis nostrum minus maxime assumenda?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ex quidem blanditiis, quae eum similique fugit inventore consectetur voluptates sunt. Mollitia autem, magnam alias rerum omnis nostrum minus maxime assumenda?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ex quidem blanditiis, quae eum similique fugit inventore consectetur voluptates sunt. Mollitia autem, magnam alias rerum omnis nostrum minus maxime assumenda?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ex quidem blanditiis, quae eum similique fugit inventore consectetur voluptates sunt. Mollitia autem, magnam alias rerum omnis nostrum minus maxime assumenda?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ex quidem blanditiis, quae eum similique fugit inventore consectetur voluptates sunt. Mollitia autem, magnam alias rerum omnis nostrum minus maxime assumenda?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ex quidem blanditiis, quae eum similique fugit inventore consectetur voluptates sunt. Mollitia autem, magnam alias rerum omnis nostrum minus maxime assumenda?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ex quidem blanditiis, quae eum similique fugit inventore consectetur voluptates sunt. Mollitia autem, magnam alias rerum omnis nostrum minus maxime assumenda?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ex quidem blanditiis, quae eum similique fugit inventore consectetur voluptates sunt. Mollitia autem, magnam alias rerum omnis nostrum minus maxime assumenda?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ex quidem blanditiis, quae eum similique fugit inventore consectetur voluptates sunt. Mollitia autem, magnam alias rerum omnis nostrum minus maxime assumenda?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ex quidem blanditiis, quae eum similique fugit inventore consectetur voluptates sunt. Mollitia autem, magnam alias rerum omnis nostrum minus maxime assumenda?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ex quidem blanditiis, quae eum similique fugit inventore consectetur voluptates sunt. Mollitia autem, magnam alias rerum omnis nostrum minus maxime assumenda?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ex quidem blanditiis, quae eum similique fugit inventore consectetur voluptates sunt. Mollitia autem, magnam alias rerum omnis nostrum minus maxime assumenda?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ex quidem blanditiis, quae eum similique fugit inventore consectetur voluptates sunt. Mollitia autem, magnam alias rerum omnis nostrum minus maxime assumenda?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ex quidem blanditiis, quae eum similique fugit inventore consectetur voluptates sunt. Mollitia autem, magnam alias rerum omnis nostrum minus maxime assumenda?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ex quidem blanditiis, quae eum similique fugit inventore consectetur voluptates sunt. Mollitia autem, magnam alias rerum omnis nostrum minus maxime assumenda?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ex quidem blanditiis, quae eum similique fugit inventore consectetur voluptates sunt. Mollitia autem, magnam alias rerum omnis nostrum minus maxime assumenda?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ex quidem blanditiis, quae eum similique fugit inventore consectetur voluptates sunt. Mollitia autem, magnam alias rerum omnis nostrum minus maxime assumenda?
+    </p>
+  </div>
 </template>
+
+<style scoped lang="scss">
+  .content {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    overflow-y: auto;
+  }
+</style>
