b1e10ae602f4fdd19e20bf11e594e2b21c777515
Simon Baev <simon.baev@bsci.com>
2023-12-14 10:16:29 -0700

    init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <RouterView class="viewport" />
+</template>
+
+<style scoped>
+  .viewport {
+    height: 100vh;
+  }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <h2>Hello</h2>
+  </main>
+</template>
